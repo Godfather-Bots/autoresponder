59e4bcd2732ede37de2884e4e7ef612bab9294b9
Mark Struchkov <upagge@icloud.com>
2019-01-14 15:35:53 +0300

    Release 1.0
    Первый релиз. Возможности
    1) Автоматический ответ на выбор по ключевым словам пользователя
    2) Вставка слов в ответ от пользоваетля
    3) Добавление пользователя, если его не было
    4) Формирование меню

--- src/main/java/org/sadtech/autoresponder/service/PersonService.java
+++ src/main/java/org/sadtech/autoresponder/service/PersonService.java
@@ -5,7 +5,9 @@ import org.sadtech.autoresponder.entity.Person;
 public interface PersonService {
 
     Person getPersonById(Integer integer);
+
     void addPerson(Person person);
+
     boolean checkPerson(Integer idPerson);
 
 }
--- src/main/java/org/sadtech/autoresponder/service/UnitService.java
+++ src/main/java/org/sadtech/autoresponder/service/UnitService.java
@@ -3,6 +3,7 @@ package org.sadtech.autoresponder.service;
 import org.sadtech.autoresponder.entity.Unit;
 
 public interface UnitService {
+
     Unit nextUnit(Unit unit, String message);
 
     Unit getUnitById(Integer idUnit);
