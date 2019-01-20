2a057e6b67b2226267cfaf053b0c2e5b3af97bff
Mark Struchkov <upagge@icloud.com>
2019-01-21 00:35:52 +0300

    Поправил конфигуратор и написал основной тест

--- src/main/java/org/sadtech/autoresponder/entity/compare/UnitPriorityComparator.java
+++ src/main/java/org/sadtech/autoresponder/entity/compare/UnitPriorityComparator.java
@@ -8,7 +8,7 @@ public class UnitPriorityComparator implements Comparator<Unit> {
 
     @Override
     public int compare(Unit o1, Unit o2) {
-        if (o1.getPriority() > o2.getPriority()) {
+        if (o1.getPriority() < o2.getPriority()) {
             return -1;
         } else if (o1.getPriority().equals(o2.getPriority())) {
             return 0;
