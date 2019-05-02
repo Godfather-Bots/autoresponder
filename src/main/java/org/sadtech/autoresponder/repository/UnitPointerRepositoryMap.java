61364e9783070c41be46a4171de9627693c4a93e
Mark Struchkov <upagge@icloud.com>
2019-05-02 20:55:11 +0300

    Рефакторинг и ренейминг

---
+++ src/main/java/org/sadtech/autoresponder/repository/UnitPointerRepositoryMap.java
@@ -0,0 +1,32 @@
+package org.sadtech.autoresponder.repository;
+
+import org.sadtech.autoresponder.entity.UnitPointer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UnitPointerRepositoryMap implements UnitPointerRepository {
+
+    private Map<Integer, UnitPointer> unitPointerMap = new HashMap<>();
+
+    @Override
+    public Integer add(UnitPointer unitPointer) {
+        unitPointerMap.put(unitPointer.getEntityId(), unitPointer);
+        return unitPointer.getEntityId();
+    }
+
+    @Override
+    public void remove(Integer entityId) {
+        unitPointerMap.remove(entityId);
+    }
+
+    @Override
+    public void addAll(Map<Integer, UnitPointer> unitPointerMap) {
+        this.unitPointerMap.putAll(unitPointerMap);
+    }
+
+    @Override
+    public UnitPointer findByEntityId(Integer entityId) {
+        return unitPointerMap.get(entityId);
+    }
+}
